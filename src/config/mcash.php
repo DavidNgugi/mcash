56a66b60117e1f2fc031efd197d11a5e90384381
Darth <ayimdomnic@outlook.com>
2017-08-03 14:30:21 +0300

    Added Guzzle and a config file

--- src/config/mcash.php
+++ src/config/mcash.php
@@ -1,1 +1,34 @@
 <?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication (Consumer Key)
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the default authentication consumer key
+    | from your developer account. Go to https://developer.safaricom.co.ke
+    | create an application and get the consumer key
+    |
+    */
+
+    'consumer_key' => env('YOUR_CONSUMER_KEY', 'some_random_String'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication (Consumer_secret)
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the default authentication consumer secret
+    | from your developer account. Go to https://developer.safaricom.co.ke
+    | create an application and get the consumer secret
+    |
+    */
+
+    'consumer_secret' => env('YOUR_CONSUMER_SECRET', 'some_random_string'),
+    
+
+   
+
+];
